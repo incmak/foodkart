bcd109a20abef62d7aad72783633589b62e56e5d
MAK <ahmedmuieen@gmail.com>
2020-06-13 11:09:41 +0530

    intial browse pic

--- add_food_items.php
+++ add_food_items.php
@@ -108,7 +108,7 @@ header('Location: managerlogin.php'); // Redirecting To Home Page
     
     <div class="col-xs-9">
       <div class="form-area" style="padding: 0px 100px 100px 100px;">
-        <form action="add_food_items1.php" method="POST">
+        <form action="add_food_items1.php" method="POST" enctype="multipart/form-data">
         <br style="clear: both">
           <h3 style="margin-bottom: 25px; text-align: center; font-size: 30px;"> ADD NEW FOOD ITEM HERE </h3>
 
@@ -125,7 +125,7 @@ header('Location: managerlogin.php'); // Redirecting To Home Page
           </div>
 
           <div class="form-group">
-            <input type="text" class="form-control" id="images_path" name="images_path" placeholder="Your Food Image Path [images/<filename>.<extention>]" required="">
+            <input type="file" class="form-control" id="images_path" name="images_path" placeholder="Your Food Image Path [images/<filename>.<extention>]" required="">
           </div>
 
           <div class="form-group">
